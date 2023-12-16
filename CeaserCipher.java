b0f6545f56ec30c236f90c0f821db2ed0b73d9da
Spikey <138967412+Spikree@users.noreply.github.com>
2023-12-16 15:40:19 +0530

    Add files via upload

--- CeaserCipher.java
+++ CeaserCipher.java
@@ -1,45 +1,99 @@
 import java.util.Scanner;
 
 public class CeaserCipher {
-    char[] letters = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    };
-
-    String choice;
-    String direction;
-    String text;
-    int shift;
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Do you wanna encrypt or decrypt your message?");
-        String choice = sc.nextLine();
-        String lowerChoice = choice.toLowerCase();
-
-        if (lowerChoice == "encrypt") {
-            System.out.println("What direction do you wanna encrypt your text in? (left or right)");
-            String direction = sc.nextLine().toLowerCase();
-
-            System.out.println("Enter the text you wanna encrypt");
-            String text = sc.nextLine();
-
-            System.out.println("Enter the Shift amount");
-            int shift = sc.nextInt();
-
-        } else if (lowerChoice == "decrypt") {
-            System.out.println("What direction do you wanna decrypt your text in (must be the same that you used while encryypting)");
-            String direction = sc.nextLine().toLowerCase();
-
-            System.out.println("Enter the text you want to encrypt");
-            String text = sc.nextLine();
-
-            System.out.println("Enter the Shift amount");
-            int shift = sc.nextInt();
+        System.out.println("Type 'encode' to encrypt, type 'decode' to decrypt:");
+        String direction = scanner.nextLine();
+        System.out.println("Type your message:");
+        String text = scanner.nextLine().toLowerCase();
+        
+        int shift;
+        try {
+            System.out.println("Type the shift number:");
+            shift = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Invalid input for shift. Please enter an integer.");
+            return;
         }
 
-        static String ceaser(String lowerChoice, String direction, String text, int shift) {
+        scanner.nextLine();
 
+        shift = shift % 26;
+
+        if (direction.equals("encode")) {
+            String cipherText = encrypt(text, alphabet, shift);
+            System.out.println("The new encrypted message is " + cipherText);
+        } else if (direction.equals("decode")) {
+            String decryptedText = decrypt(text, alphabet, shift);
+            System.out.println("The decrypted message is " + decryptedText);
         }
+
+        System.out.println("Do you want to continue using the program? Type 'yes' or 'no': ");
+        String userInput = scanner.next().toLowerCase();
+        while (!userInput.equals("no")) {
+            System.out.println("Type 'encode' to encrypt, type 'decode' to decrypt:");
+            direction = scanner.next();
+            scanner.nextLine(); 
+            System.out.println("Type your message:");
+            text = scanner.nextLine().toLowerCase();
+            
+            try {
+                System.out.println("Type the shift number:");
+                shift = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input for shift. Please enter an integer.");
+                return;
+            }
+
+            scanner.nextLine(); 
+
+            shift = shift % 26;
+
+            if (direction.equals("encode")) {
+                String cipherText = encrypt(text, alphabet, shift);
+                System.out.println("The new encrypted message is " + cipherText);
+            } else if (direction.equals("decode")) {
+                String decryptedText = decrypt(text, alphabet, shift);
+                System.out.println("The decrypted message is " + decryptedText);
+            }
+
+            System.out.println("Do you want to continue using the program? Type 'yes' or 'no': ");
+            userInput = scanner.next().toLowerCase();
+        }
+
+        System.out.println("Goodbye!");
+    }
+
+    private static String encrypt(String plainText, char[] alphabet, int shiftAmount) {
+        StringBuilder cipherText = new StringBuilder();
+        for (char letter : plainText.toCharArray()) {
+            if (Character.isLetter(letter)) {
+                int position = new String(alphabet).indexOf(letter);
+                int newPosition = (position + shiftAmount) % 26;
+                char newLetter = alphabet[newPosition];
+                cipherText.append(newLetter);
+            } else {
+                cipherText.append(letter);
+            }
+        }
+        return cipherText.toString();
+    }
+
+    private static String decrypt(String cipherText, char[] alphabet, int shiftAmount) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (char letter : cipherText.toCharArray()) {
+            if (Character.isLetter(letter)) {
+                int position = new String(alphabet).indexOf(letter);
+                int newPosition = (position - shiftAmount + 26) % 26; 
+                char newLetter = alphabet[newPosition];
+                decryptedText.append(newLetter);
+            } else {
+                decryptedText.append(letter);
+            }
+        }
+        return decryptedText.toString();
     }
 }
